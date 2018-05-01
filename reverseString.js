fc9b77e04977dc9e210ce2b302aa0e80a607c537
Augustas Skidzevičius <34551243+augisdh@users.noreply.github.com>
2018-05-01 23:42:29 +0300

    init commit with comments

--- reverseString.js
+++ reverseString.js
@@ -1,11 +1,11 @@
 function spinWords(string){
-    const array = string.split(" ");
+    const array = string.split(" "); // transform String to Array
     let text = "";
     
     for(i = 0; i < array.length; i++){
       const word = array[i];
       if(word.length >= 5){
-        text += word.split("").reverse().join("").toString() + " ";
+        text += word.split("").reverse().join("").toString() + " "; // Reverse letters if word contains 5 or more letters
       } else {
         text += array[i] + " ";
       }

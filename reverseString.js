81e090a2ec45cbeea93e098e34df7eeb969ed6cc
Augustas Skidzevičius <34551243+augisdh@users.noreply.github.com>
2018-05-02 17:56:05 +0300

    let missing

--- reverseString.js
+++ reverseString.js
@@ -2,7 +2,7 @@ function spinWords(string){
     const array = string.split(" "); // transform String to Array
     let text = "";
     
-    for(i = 0; i < array.length; i++){
+    for(let i = 0; i < array.length; i++){
       const word = array[i];
       if(word.length >= 5){
         text += word.split("").reverse().join("").toString() + " "; // Reverse letters if word contains 5 or more letters
